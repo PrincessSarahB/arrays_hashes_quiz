c09b56316bfbb4bb29d9d2b4688ae0bca69e5234
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-03-28 15:54:41 +0100

    more q's answered

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -15,10 +15,13 @@ stops.unshift("Glasgow Queen St")
 
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
 stops.insert(4, "Polmont")
-p stops
 # 4. Work out the index position of `"Linlithgow"`
+
 # 5. Remove `"Livingston"` from the array using its name
+stops.delete("Livingston")
 # 6. Delete `"Cumbernauld"` from the array by index
+stops.delete_at(2)
+p stops
 # 7. How many stops there are in the array?
 # 8. How many ways can we return `"Falkirk High"` from the array?
 # 9. Reverse the positions of the stops in the array
